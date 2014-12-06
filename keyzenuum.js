08f38e388128dd1c90e10530a97d608ba392cdbf
behahm <bensaad.ahmed@live.fr>
2014-12-06 17:30:51 +0100

    + controls
    zoom & restart

--- keyzenuum.js
+++ keyzenuum.js
@@ -1,15 +1,15 @@
 var data = {};
-var options = {};
-var hits_correct = 0;
-var hits_wrong = 0;
-var start_time = 0;
-var hpm = 0;
-var ratio = 0;
 
 // Extrait du roman d'Albert Camus "L'Etranger"
 data.chars = "Aujourd\'hui, maman est morte. Ou peut-\u00eatre hier, je ne sais pas. J\'ai re\u00e7u un t\u00e9l\u00e9gramme de l\'asile: \u00abM\u00e8re d\u00e9c\u00e9d\u00e9e. Enterrement demain. Sentiments distingu\u00e9s.\u00bb Cela ne veut rien dire. C\'\u00e9tait peut-\u00eatre hier. L\'asile de vieillards est \u00e0 Marengo, \u00e0 quatre-vingts kilom\u00e8tres d\'Alger. Je prendrai l\'autobus \u00e0 deux heures et j\'arriverai dans l\'apr\u00e8s-midi. Ainsi, je pourrai veiller et je rentrerai demain soir. J\'ai demand\u00e9 deux jours de cong\u00e9 \u00e0 mon patron et il ne pouvait pas me les refuser avec une excuse pareille. Mais il n\'avait pas l\'air content. Je lui ai m\u00eame dit : \"Ce n\'est pas de ma faute.\" Il n\'a pas r\u00e9pondu. J\'ai pens\u00e9 alors que je n\'aurais pas d\u00fb lui dire cela. En somme, je n\'avais pas \u00e0 m\'excuser. C\'\u00e9tait plut\u00f4t \u00e0 lui de me pr\u00e9senter ses condol\u00e9ances. Mais il le fera sans doute apr\u00e8s-demain, quand il me verra en deuil. Pour le moment, c\'est un peu comme si maman n\'\u00e9tait pas morte. Apr\u00e8s l\'enterrement, au contraire, ce sera une affaire class\u00e9e et tout aura rev\u00eatu une allure plus officielle.\n\nJ\'ai pris l\'autobus \u00e0 deux heures. II faisait tr\u00e8s chaud. J\'ai mang\u00e9 au restaurant, chez C\u00e9leste, comme d\'habitude. Ils avaient tous beaucoup de peine pour moi et C\u00e9leste m\'a dit: \"On n\'a qu\'une m\u00e8re\". Quand je suis parti, ils m\'ont accompagn\u00e9 \u00e0 la porte. J\'\u00e9tais un peu \u00e9tourdi parce qu\'il a fallu que je monte chez Emmanuel pour lui emprunter une cravate noire et un brassard. Il a perdu son oncle, il y a quelques mois. J\'ai couru pour ne pas manquer le d\u00e9part. Cette h\u00e2te, cette course, c\'est \u00e0 cause de tout cela sans doute, ajout\u00e9 aux cahots, \u00e0 l\'odeur d\'essence, \u00e0 la r\u00e9verb\u00e9ration de la route et du ciel, que je me suis assoupi. J\'ai dormi pendant presque tout le trajet. Et quand je me suis r\u00e9veill\u00e9, j\'\u00e9tais tass\u00e9 contre un militaire qui m\'a souri et qui m\'a demand\u00e9 si je venais de loin. J\'ai dit \"oui\" pour n\'avoir plus \u00e0 parler.\n\nLe soir, Marie est venue me chercher et m\'a demand\u00e9 si je voulais me marier avec elle. J\'ai dit que cela m\'\u00e9tait \u00e9gal et que nous pourrions le faire si elle le voulait. Elle a voulu savoir alors si je l\'aimais. J\'ai r\u00e9pondu comme je l\'avais d\u00e9j\u00e0 fait une fois, que cela ne signifiait rien mais que sans doute je ne l\'aimais pas. \"Pourquoi m\'\u00e9pouser alors?\" a-t-elle dit. Je lui ai expliqu\u00e9 que cela n\'avait aucune importance et que si elle le d\u00e9sirait, nous pouvions nous marier. D\'ailleurs, c\'\u00e9tait elle qui le demandait et moi je me contentais de dire oui. Elle a observ\u00e9 alors que le mariage \u00e9tait une chose grave. J\'ai r\u00e9pondu : \"Non\". Elle s\'est tue un moment et elle m\'a regard\u00e9 en silence. Puis elle a parl\u00e9. Elle voulait simplement savoir si j\'aurais accept\u00e9 la m\u00eame proposition venant d\'une autre femme, \u00e0 qui je serais attach\u00e9 de la m\u00eame fa\u00e7on. J\'ai dit: \"Naturellement.\"\nElle s\'est demand\u00e9 alors si elle m\'aimait et moi, je ne pouvais rien savoir sur ce point.\nApr\u00e8s un autre moment de silence, elle a murmur\u00e9 que j\'\u00e9tais bizarre, qu\'elle m\'aimait sans doute \u00e0 cause de cela mais que peut-\u00eatre un jour je la d\u00e9go\u00fbterais pour les m\u00eames raisons. Comme je me taisais, n\'ayant rien \u00e0 ajouter, elle m\'a pris le bras en souriant et elle a d\u00e9clar\u00e9 qu\'elle voulait se marier avec moi. J\'ai r\u00e9pondu que nous le ferions d\u00e8s qu\'elle le voudrait. Je lui ai parl\u00e9 alors de la proposition du patron et Marie m\'a dit qu\'elle aimerait conna\u00eetre Paris. Je lui ai appris que j\'y avais v\u00e9cu dans un temps et elle m\'a demand\u00e9 comment c\'\u00e9tait. Je lui ai dit: \"C\'est sale. Il y a des pigeons et des cours noires. Les gens ont la peau blanche.\"\nPuis nous avons march\u00e9 et travers\u00e9 la ville par ses grandes rues. Les femmes \u00e9taient belles et j\'ai demand\u00e9 \u00e0 Marie si elle le remarquait. Elle m\'a dit que oui et qu\'elle me comprenait. Pendant un moment, nous n\'avons plus parl\u00e9. Je voulais cependant qu\'elle reste avec moi et je lui ai dit que nous pouvions d\u00eener ensemble chez C\u00e9leste. Elle en avait bien envie, mais elle avait \u00e0 faire. Nous \u00e9tions pr\u00e8s de chez moi et je lui ai dit au revoir. Elle m\'a regard\u00e9: \"Tu ne veux pas savoir ce que j\'ai \u00e0 faire?\" Je voulais bien le savoir, mais je n\'y avais pas pens\u00e9 et c\'est ce qu\'elle avait l\'air de me reprocher. Alors, devant mon air emp\u00eatr\u00e9, elle a encore ri et elle a eu vers moi un mouvement de tout le corps pour me tendre sa bouche.\n\nAu bout d\'un moment, je suis retourn\u00e9 vers la plage et je me suis mis \u00e0 marcher.\nC\'\u00e9tait le m\u00eame \u00e9clatement rouge. Sur le sable, la mer haletait de toute la respiration rapide et \u00e9touff\u00e9e de ses petites vagues. Je marchais lentement vers les rochers et je sentais mon front se gonfler sous le soleil. Toute cette chaleur s\'appuyait sur moi et s\'opposait \u00e0 mon avance. Et chaque fois que je sentais son grand souffle chaud sur mon visage, je serrais les dents, je fermais les poings dans les poches de mon pantalon, je me tendais tout entier pour triompher du soleil et de cette ivresse opaque qu\'il me d\u00e9versait. A chaque \u00e9p\u00e9e de lumi\u00e8re jaillie du sable, d\'un coquillage blanchi ou d\'un d\u00e9bris de verre, mes m\u00e2choires se crispaient. J\'ai march\u00e9 longtemps.\nJe voyais de loin la petite masse sombre du rocher entour\u00e9e d\'un halo aveuglant par la lumi\u00e8re et la poussi\u00e8re de mer. Je pensais \u00e0 la source fra\u00eeche derri\u00e8re le rocher. J\'avais envie de retrouver le murmure de son eau, envie de fuir le soleil, l\'effort et les pleurs de femme, envie enfin de retrouver l\'ombre et son repos. Mais quand j\'ai \u00e9t\u00e9 plus pr\u00e8s, j\'ai vu que le type de Raymond \u00e9tait revenu. II \u00e9tait seul. Il reposait sur le dos, les mains sous la nuque, le front dans les ombres du rocher, tout le corps au soleil. Son bleu de chauffe fumait dans la chaleur. J\'ai \u00e9t\u00e9 un peu surpris. Pour moi, c\'\u00e9tait une histoire finie et j\'\u00e9tais venu l\u00e0 sans y penser.\nD\u00e8s qu\'il m\'a vu, il s\'est soulev\u00e9 un peu et a mis la main dans sa poche. Moi, naturellement, j\'ai serr\u00e9 le revolver de Raymond dans mon veston. Alors de nouveau, il s\'est laiss\u00e9 aller en arri\u00e8re, mais sans retirer la main de sa poche. J\'\u00e9tais assez loin de lui, \u00e0 une dizaine de m\u00e8tres. Je devinais son regard par instants, entre ses paupi\u00e8res mi-closes. Mais le plus souvent, son image dansait devant mes yeux, dans l\'air enflamm\u00e9. Le bruit des vagues \u00e9tait encore plus paresseux, plus \u00e9tale qu\'\u00e0 midi. C\'\u00e9tait le m\u00eame soleil, la m\u00eame lumi\u00e8re sur le m\u00eame sable qui se prolongeait ici. Il y avait d\u00e9j\u00e0 deux heures que la journ\u00e9e n\'avan\u00e7ait plus, deux heures qu\'elle avait jet\u00e9 l\'ancre dans un oc\u00e9an de m\u00e9tal bouillant. A l\'horizon, un petit vapeur est pass\u00e9 et j\'en ai devin\u00e9 la tache noire au bord de mon regard, parce que je n\'avais pas cess\u00e9 de regarder l\'Arabe.\nJ\'ai pens\u00e9 que je n\'avais qu\'un demi-tour \u00e0 faire et ce serait fini. Mais toute une plage vibrante de soleil se pressait derri\u00e8re moi. J\'ai fait quelques pas vers la source. L\'Arabe n\'a pas boug\u00e9. Malgr\u00e9 tout, il \u00e9tait encore assez loin. Peut-\u00eatre \u00e0 cause des ombres sur son visage, il avait l\'air de rire. J\'ai attendu. La br\u00fblure du soleil gagnait mes joues et j\'ai senti des gouttes de sueur s\'amasser dans mes sourcils. C\'\u00e9tait le m\u00eame soleil que le jour o\u00f9 j\'avais enterr\u00e9 maman et, comme alors, le front surtout me faisait mal et toutes ses veines battaient ensemble sous la peau. A cause de cette br\u00fblure que je ne pouvais plus supporter, j\'ai fait un mouvement en avant. Je savais que c\'\u00e9tait stupide, que je ne me d\u00e9barrasserais pas du soleil en me d\u00e9pla\u00e7ant d\'un pas. Mais j\'ai fait un pas, un seul pas en avant. Et cette fois, sans se soulever, l\'Arabe a tir\u00e9 son couteau qu\'il m\'a pr\u00e9sent\u00e9 dans le soleil. La lumi\u00e8re a gicl\u00e9 sur l\'acier et c\'\u00e9tait comme une longue lame \u00e9tincelante qui m\'atteignait au front. Au m\u00eame instant, la sueur amass\u00e9e dans mes sourcils a coul\u00e9 d\'un coup sur les paupi\u00e8res et les a recouvertes d\'un voile ti\u00e8de et \u00e9pais. Mes yeux \u00e9taient aveugl\u00e9s derri\u00e8re ce rideau de larmes et de sel. Je ne sentais plus que les cymbales du soleil sur mon front et, indistinctement, le glaive \u00e9clatant jailli du couteau toujours en face de moi. Cette \u00e9p\u00e9e br\u00fblante rongeait mes cils et fouillait mes yeux douloureux. C\'est alors que tout a vacill\u00e9. La mer a charri\u00e9 un souffle \u00e9pais et ardent. Il m\'a sembl\u00e9 que le ciel s\'ouvrait sur toute son \u00e9tendue pour laisser pleuvoir du feu. Tout mon \u00eatre s\'est tendu et j\'ai crisp\u00e9 ma main sur le revolver. La g\u00e2chette a c\u00e9d\u00e9, j\'ai touch\u00e9 le ventre poli de la crosse et c\'est l\u00e0, dans le bruit \u00e0 la fois sec et assourdissant, que tout a commenc\u00e9. J\'ai secou\u00e9 la sueur et le soleil. J\'ai compris que j\'avais d\u00e9truit l\'\u00e9quilibre du jour, le silence exceptionnel d\'une plage o\u00f9 j\'avais \u00e9t\u00e9 heureux. Alors, j\'ai tir\u00e9 encore quatre fois sur un corps inerte o\u00f9 les balles s\'enfon\u00e7aient sans qu\'il y par\u00fbt. Et c\'\u00e9tait comme quatre coups brefs que je frappais sur la porte du malheur.\n\nL\'apr\u00e8s-midi, les grands ventilateurs brassaient toujours l\'air \u00e9pais de la salle et les petits \u00e9ventails multicolores des jur\u00e9s s\'agitaient tous dans le m\u00eame sens. La plaidoirie de mon avocat me semblait ne devoir jamais finir. A un moment donn\u00e9, cependant, je l\'ai \u00e9cout\u00e9 parce qu\'il disait: \"Il est vrai que j\'ai tu\u00e9\". Puis il a continu\u00e9 sur ce ton, disant \"je\" chaque fois qu\'il parlait de moi. J\'\u00e9tais tr\u00e8s \u00e9tonn\u00e9. Je me suis pench\u00e9 vers un gendarme et je lui ai demand\u00e9 pourquoi. Il m\'a dit de me taire et, apr\u00e8s un moment, il a ajout\u00e9: \"Tous les avocats font \u00e7a.\" Moi, j\'ai pens\u00e9 que c\'\u00e9tait m\'\u00e9carter encore de l\'affaire, me r\u00e9duire \u00e0 z\u00e9ro et, en un certain sens, se substituer \u00e0 moi. Mais je crois que j\'\u00e9tais d\u00e9j\u00e0 tr\u00e8s loin de cette salle d\'audience. D\'ailleurs, mon avocat m\'a sembl\u00e9 ridicule. Il a plaid\u00e9 la provocation tr\u00e8s rapidement et puis lui aussi a parl\u00e9 de mon \u00e2me. Mais il m\'a paru qu\'il avait beaucoup moins de talent que le procureur. \"Moi aussi, a-t-il dit, je me suis pench\u00e9 sur cette \u00e2me, mais, contrairement \u00e0 l\'\u00e9minent repr\u00e9sentant du minist\u00e8re public, j\'ai trouv\u00e9 quelque chose et je puis dire que j\'y ai lu \u00e0 livre ouvert.\" II y avait lu que j\'\u00e9tais un honn\u00eate homme, un travailleur r\u00e9gulier, infatigable, fid\u00e8le \u00e0 la maison qui l\'employait, aim\u00e9 de tous et compatissant aux mis\u00e8res d\'autrui. Pour lui, j\'\u00e9tais un fils mod\u00e8le qui avait soutenu sa m\u00e8re aussi longtemps qu\'il l\'avait pu. Finalement j\'avais esp\u00e9r\u00e9 qu\'une maison de retraite donnerait \u00e0 la vieille femme le confort que mes moyens ne me permettaient pas de lui procurer. \"Je m\'\u00e9tonne, messieurs, a-t-il ajout\u00e9, qu\'on ait men\u00e9 si grand bruit autour de cet asile. Car enfin, s\'il fallait donner une preuve de l\'utilit\u00e9 et de la grandeur de ces institutions, il faudrait bien dire que c\'est l\'Etat lui-m\u00eame qui les subventionne.\" Seulement, il n\'a pas parl\u00e9 de l\'enterrement et j\'ai senti que cela manquait dans sa plaidoirie. Mais \u00e0 cause de toutes ces longues phrases, de toutes ces journ\u00e9es et ces heures interminables pendant lesquelles on avait parl\u00e9 de mon \u00e2me, j\'ai eu l\'impression que tout devenait comme une eau incolore o\u00f9 je trouvais le vertige. A la fin, je me souviens seulement que, de la rue et \u00e0 travers tout l\'espace des salles et des pr\u00e9toires, pendant que mon avocat continuait \u00e0 parler, la trompette d\'un marchand de glace a r\u00e9sonn\u00e9 jusqu\'\u00e0 moi. J\'ai \u00e9t\u00e9 assailli des souvenirs d\'une vie qui ne m\'appartenait plus, mais o\u00f9 j\'avais trouv\u00e9 les plus pauvres et les plus tenaces de mes joies: des odeurs d\'\u00e9t\u00e9, le quartier que j\'aimais, un certain ciel du soir, le rire et les robes de Marie. Tout ce que je faisais d\'inutile en ce lieu m\'est alors remont\u00e9 \u00e0 la gorge et je n\'ai eu qu\'une h\u00e2te, c\'est qu\'on en finisse et que je retrouve ma cellule avec le sommeil. C\'est \u00e0 peine si j\'ai entendu mon avocat s\'\u00e9crier, pour finir, que les jur\u00e9s ne voudraient pas envoyer \u00e0 la mort un travailleur honn\u00eate perdu par une minute d\'\u00e9garement, et demander les circonstances att\u00e9nuantes pour un crime dont je tra\u00eenais d\u00e9j\u00e0, comme le plus s\u00fbr de mes ch\u00e2timents, le remords \u00e9ternel. La cour a suspendu l\'audience et l\'avocat s\'est assis d\'un air \u00e9puis\u00e9. Mais ses coll\u00e8gues sont venus vers lui pour lui serrer la main. J\'ai entendu: \"Magnifique, mon cher.\" L\'un d\'eux m\'a m\u00eame pris \u00e0 t\u00e9moin: \"Hein?\" m\'a-t-il dit. J\'ai acquiesc\u00e9, mais mon compliment n\'\u00e9tait pas sinc\u00e8re, parce que j\'\u00e9tais trop fatigu\u00e9.\n\nLui parti, j\'ai retrouv\u00e9 le calme. J\'\u00e9tais \u00e9puis\u00e9 et je me suis jet\u00e9 sur ma couchette. Je crois que j\'ai dormi parce que je me suis r\u00e9veill\u00e9 avec des \u00e9toiles sur le visage. Des bruits de campagne montaient jusqu\'\u00e0 moi. Des odeurs de nuit, de terre et de sel rafra\u00eechissaient mes tempes. La merveilleuse paix de cet \u00e9t\u00e9 endormi entrait en moi comme une mar\u00e9e. A ce moment, et \u00e0 la limite de la nuit, des sir\u00e8nes ont hurl\u00e9. Elles annon\u00e7aient des d\u00e9parts pour un monde qui maintenant m\'\u00e9tait \u00e0 jamais indiff\u00e9rent.\nPour la premi\u00e8re fois depuis bien longtemps, j\'ai pens\u00e9 \u00e0 maman. Il m\'a sembl\u00e9 que je comprenais pourquoi \u00e0 la fin d\'une vie elle avait pris un \u00abfianc\u00e9\u00bb, pourquoi elle avait jou\u00e9 \u00e0 recommencer. L\u00e0-bas, l\u00e0-bas aussi, autour de cet asile o\u00f9 des vies s\'\u00e9teignaient, le soir \u00e9tait comme une tr\u00eave m\u00e9lancolique. Si pr\u00e8s de la mort, maman devait s\'y sentir lib\u00e9r\u00e9e et pr\u00eate \u00e0 tout revivre. Personne, personne n\'avait le droit de pleurer sur elle. Et moi aussi, je me suis senti pr\u00eat \u00e0 tout revivre. Comme si cette\ngrande col\u00e8re m\'avait purg\u00e9 du mal, vid\u00e9 d\'espoir, devant cette nuit charg\u00e9e de signes et d\'\u00e9toiles, je m\'ouvrais pour la premi\u00e8re fois \u00e0 la tendre indiff\u00e9rence du monde. De l\'\u00e9prouver si pareil \u00e0 moi, si fraternel enfin, j\'ai senti que j\'avais \u00e9t\u00e9 heureux, et que je l\'\u00e9tais encore. Pour que tout soit consomm\u00e9, pour que je me sente moins seul, il me restait \u00e0 souhaiter qu\'il y ait beaucoup de spectateurs le jour de mon ex\u00e9cution et qu\'ils m\'accueillent avec des cris de haine.";
 data.chars_index = 0;
 data.word_length = 36;
+data.hits_correct = 0;
+data.hits_wrong = 0;
+data.start_time = 0;
+data.hpm = 0;
+data.ratio = 0;
+data.font_size=56;
 
 
 $(document).ready(function() {
@@ -25,24 +25,27 @@ $(document).ready(function() {
     $(document).keydown(specialKeyHandler);
     // handle keyboard press
     $(document).keypress(keyHandler);
+
+    // handle control
+    control()
 });
 
 
 function start_stats() {
-    start_time = start_time || Math.floor(new Date().getTime() / 1000);
+    data.start_time = data.start_time || Math.floor(new Date().getTime() / 1000);
 }
 
 
 function update_stats() {
-    if (start_time) {
+    if (data.start_time) {
         var current_time = (Math.floor(new Date().getTime() / 1000));
-        ratio = Math.floor(
-            hits_correct / (hits_correct + hits_wrong) * 100
+        data.ratio = Math.floor(
+            data.hits_correct / (data.hits_correct + data.hits_wrong) * 100
         );
-        hpm = Math.floor(
-            (hits_correct + hits_wrong) / (current_time - start_time) * 60
+        data.hpm = Math.floor(
+            (data.hits_correct + data.hits_wrong) / (current_time - data.start_time) * 60
         );
-        if (!isFinite(hpm)) { hpm = 0; }
+        if (!isFinite(data.hpm)) { data.hpm = 0; }
     }
 }
 
@@ -59,11 +62,11 @@ function start() {
 
 
 function restart(){
-    hits_correct = 0;
-    hits_wrong = 0;
-    start_time = 0;
-    hpm = 0;
-    ratio = 0;
+    data.hits_correct = 0;
+    data.hits_wrong = 0;
+    data.start_time = 0;
+    data.hpm = 0;
+    data.ratio = 0;
     start();
 }
 
@@ -106,12 +109,12 @@ function keyHandler(e) {
     }
     
     if(data.keys_hit.length==data.word_index && key == data.word[data.word_index]) {
-        hits_correct += 1;
+        data.hits_correct += 1;
         (new Audio("click.wav")).play();
     }
     else {
 
-        hits_wrong += 1;
+        data.hits_wrong += 1;
         (new Audio("clack.wav")).play();
         data.word_errors[data.word_index] = true;
         
@@ -156,21 +159,21 @@ function load() {
 
 function render() {
     render_word();
-    //render_rigor();
     render_stats();
 }
 
 
 function render_stats() {
     $("#stats").text([
-        "hits per minute: ", hpm, " ",
-        "correctness: ", ratio, "%"
+        "hits per minute: ", data.hpm, " ",
+        "correctness: ", data.ratio, "%"
     ].join(""));
 }
 
  
 
 function render_word() {
+    setFontSize(data.font_size);
     var word = "";
     for (var i = 0; i < data.word.length; i++) {
         
@@ -225,8 +228,36 @@ function generate_word() {
 }
 
 
+function control() {
+    $("#control > #restart").click(function(e){
+        start();
+    });
 
-// minus 1 till Zero
-function minus1(num) {
-    return num>0?num-1:0;
+    $("#control > #zoomPlus").click(function(e){
+        data.font_size=plusXTillY(data.font_size,4,72);
+        setFontSize(data.font_size);
+    });
+
+    $("#control > #zoomMinus").click(function(e){
+        data.font_size=minusXTillY(data.font_size,4,32);
+        setFontSize(data.font_size);
+    });
+
+    
 }
+
+function setFontSize(fontSize){
+    $("#word").css("font-size",data.font_size+"px");
+    $(".keys-hit").css("font-size",data.font_size+"px");
+}
+
+// minus X till Y
+function minusXTillY(num,x,y) {
+    return num>y?num-x:y;
+}
+
+// plus X till Y
+function plusXTillY(num,x,y) {
+    return num<y?num+x:y;
+}
+
